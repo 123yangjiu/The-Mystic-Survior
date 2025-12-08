66240ac4580f7d339cf205f62db9a669903f08cc
monolith-Epee <3572871617@qq.com>
2025-12-08 15:55:52 +0800

    修复锤子场景为光剑，并更改增伤机制
    修复锤子场景为光剑，并更改增伤机制

--- script/upgrade/hammer_ability_controller.gd
+++ script/upgrade/hammer_ability_controller.gd
@@ -2,76 +2,68 @@ extends Node
 const MAX_RANGE=200
 @export var hammer_ability:PackedScene
 
-var Damage=28#定义的伤害
+var Damage=30#定义的伤害
 var base_wait_time#定义基础冷却
-
+var base_scale=1#定义光剑基础大小
 
 func _ready() -> void:
-	base_wait_time=$Timer.wait_time
-	$Timer.timeout.connect(on_timer_timeoout)
-	GameEvent.ability_upgrade_add.connect(on_ability_upgrade_add)
-	
+    base_wait_time=$Timer.wait_time
+    $Timer.timeout.connect(on_timer_timeoout)
+    GameEvent.ability_upgrade_add.connect(on_ability_upgrade_add)
+    
 
 func on_timer_timeoout():
-	
-	var player=get_tree().get_first_node_in_group("player") as Player
-	var enemies= get_tree().get_nodes_in_group("enemy")
-	enemies=enemies.filter(func(enemy:Node2D):#过滤掉不在范围内的敌人
-		return enemy.global_position.distance_squared_to(player.global_position)<pow(MAX_RANGE,2)
-	
-	)
-	#get_first_node_in_group是拿到组里面的第一个节点，如果我们想要拿到player节点
-	#就要在player脚本中加入add_to_group("player")
-	enemies.sort_custom(func (a:Node2D,b:Node2D):
-		var A_distance=a.global_position.distance_squared_to(player.global_position)
-		var B_distance=b.global_position.distance_squared_to(player.global_position)
-		return A_distance< B_distance
-	
-		
-	)
-	
-	
-	var hammer_instance = hammer_ability.instantiate() as Hammerability
-	var foreground = get_tree().get_first_node_in_group("前景图层")
+    
+    var player=get_tree().get_first_node_in_group("player") as Player
+    var enemies= get_tree().get_nodes_in_group("enemy")
+    enemies=enemies.filter(func(enemy:Node2D):#过滤掉不在范围内的敌人
+        return enemy.global_position.distance_squared_to(player.global_position)<pow(MAX_RANGE,2)
+    
+    )
+    #get_first_node_in_group是拿到组里面的第一个节点，如果我们想要拿到player节点
+    #就要在player脚本中加入add_to_group("player")
+    enemies.sort_custom(func (a:Node2D,b:Node2D):
+        var A_distance=a.global_position.distance_squared_to(player.global_position)
+        var B_distance=b.global_position.distance_squared_to(player.global_position)
+        return A_distance< B_distance
+    
+        
+    )
+    
+    
+    var hammer_instance = hammer_ability.instantiate() as holy_right_ruling
+    var foreground = get_tree().get_first_node_in_group("前景图层")
+    hammer_instance.scale*=base_scale
 
-		# ② 等节点_ready完成再赋值，避免 nil
-	foreground.add_child(hammer_instance)#加入到场景中
-	hammer_instance.hitbox_component.damage =int(Damage+randf_range(-5,5))
-	if enemies.is_empty():
-		hammer_instance.queue_free()
-		return
-		
-		 # ③ 位置 & 朝向
-	hammer_instance.global_position =player.global_position
-	if player.direction.x>0:
-		hammer_instance.get_node("AnimationPlayer").play("hammer_right")
-	if player.direction.x<0:
-		hammer_instance.get_node("AnimationPlayer").play("hammer_left")
-	if player.direction.x==0:
-		if randf()>0.5:
-			hammer_instance.get_node("AnimationPlayer").play("hammer_left")
-		else:
-			hammer_instance.get_node("AnimationPlayer").play("hammer_right")
-		
+        # ② 等节点_ready完成再赋值，避免 nil
+    foreground.add_child(hammer_instance)#加入到场景中
+    hammer_instance.hitbox_component.damage =int(Damage+randf_range(-5,5))
+    if enemies.is_empty():
+        hammer_instance.queue_free()
+        return
+        
+         # ③ 位置 & 朝向
+    hammer_instance.global_position =player.global_position
+        
 
 # 再拿最近的那只
-	
+    
 
-	
+    
 func on_ability_upgrade_add(upgrade:AbilityUpgrade,current_upgrade:Dictionary):
-	#监听所有关于剑的升级
-	print("解锁成功")
-	if upgrade.ID=="锤的速度":
-		var persent_reduction=current_upgrade["锤的速度"]["quantity"]*.2
-		$Timer.wait_time=base_wait_time*(1-persent_reduction)
-		$Timer.start()
-	if upgrade.ID=="锤的力量":
-		var persent_improvement=current_upgrade["锤的力量"]["quantity"]*.2
-		Damage=Damage*(1+persent_improvement)
-		print(Damage)
-	if upgrade.ID=="解锁战锤":
-		$Timer.start()
-		
-		#print("解锁战锤")
-		
-	#pass	
+    #监听所有关于剑的升级
+    print("解锁成功")
+    if upgrade.ID=="光的速度":
+        var persent_reduction=current_upgrade["锤的速度"]["quantity"]*.2
+        $Timer.wait_time=base_wait_time*(1-persent_reduction)
+        $Timer.start()
+    if upgrade.ID=="光的力量":
+        Damage*=1.2
+        print(Damage)
+    if upgrade.ID=="解锁光剑":
+        $Timer.start()
+    if upgrade.ID=="光剑变大":
+        base_scale+=0.2
+        #print("解锁战锤")
+        
+    #pass	
--- 特殊掉落.gd
+++ 特殊掉落.gd
@@ -7,34 +7,34 @@ extends Node
 var random_R=randf_range(100,150)
 var bonus_drop
 func _ready() -> void:
-	(health_component as HealthComponent).died.connect(on_died)
+    (health_component as HealthComponent).died.connect(on_died)
 func on_died():
-	var bonus_botlle:=0
-	for i in range(5):
-		var angle := TAU * i / 3
-		var entities_Layer=get_tree().get_first_node_in_group("实体图层")
-		var spawn_position=(owner as Node2D).global_position
-		var bottle_instence:Node2D
-		if randf()<drop_persent:
-			#普通经验瓶
-			bottle_instence=drop_thing[0].instantiate() as Node2D
-			call_deferred("add_bottle",entities_Layer,spawn_position,angle,bottle_instence)
-			if i==4 and bonus_botlle==0:
-				bottle_instence=drop_thing[2].instantiate() as Node2D
-				call_deferred("add_bottle",entities_Layer,spawn_position,angle,bottle_instence)
-				bottle_instence=drop_thing[1].instantiate() as Node2D
-				call_deferred("add_bottle",entities_Layer,spawn_position,angle,bottle_instence)
-				return
-			if randf()<drop_blood_persent:
-				bottle_instence=drop_thing[1].instantiate() as Node2D
-			else:
-				bonus_botlle +=1
-				bottle_instence=drop_thing[2].instantiate() as Node2D
-			call_deferred("add_bottle",entities_Layer,spawn_position,angle,bottle_instence)
-		
+    var bonus_botlle:=0
+    for i in range(5):
+        var angle := TAU * i / 3
+        var entities_Layer=get_tree().get_first_node_in_group("实体图层")
+        var spawn_position=(owner as Node2D).global_position
+        var bottle_instence:Node2D
+        if randf()<drop_persent:
+            #普通经验瓶
+            bottle_instence=drop_thing[0].instantiate() as Node2D
+            call_deferred("add_bottle",entities_Layer,spawn_position,angle,bottle_instence)
+            if i==4 and bonus_botlle==0:#保底掉落一个bonus_bottle
+                bottle_instence=drop_thing[2].instantiate() as Node2D
+                call_deferred("add_bottle",entities_Layer,spawn_position,angle,bottle_instence)
+                bottle_instence=drop_thing[1].instantiate() as Node2D
+                call_deferred("add_bottle",entities_Layer,spawn_position,angle,bottle_instence)
+                return
+            if randf()<drop_blood_persent:
+                bottle_instence=drop_thing[1].instantiate() as Node2D
+            else:
+                bonus_botlle +=1
+                bottle_instence=drop_thing[2].instantiate() as Node2D
+            call_deferred("add_bottle",entities_Layer,spawn_position,angle,bottle_instence)
+        
 
 func add_bottle(entities_Layer:Node,spawn_position:Vector2,angle,bottle_instance:Node2D):
-	if ! bottle_instance:
-		return
-	entities_Layer.add_child(bottle_instance)
-	bottle_instance.global_position=spawn_position+Vector2.RIGHT.rotated(angle) *float(random_R)
+    if ! bottle_instance:
+        return
+    entities_Layer.add_child(bottle_instance)
+    bottle_instance.global_position=spawn_position+Vector2.RIGHT.rotated(angle) *float(random_R)
