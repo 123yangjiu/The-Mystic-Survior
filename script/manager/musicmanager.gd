24aa0814d42685dc3e470a57dc8c297aaabfc83b
monolith-Epee <3572871617@qq.com>
2025-12-12 15:20:06 +0800

    缩进小问题
    缩进

--- script/manager/musicmanager.gd
+++ script/manager/musicmanager.gd
@@ -10,29 +10,29 @@ extends Node
 
 
 func _ready() -> void:
-	GameEvent.more_difficulty.connect(on_more_difficulty)
-	GameEvent.the_first_damage.connect(on_the_first_damage)
+    GameEvent.more_difficulty.connect(on_more_difficulty)
+    GameEvent.the_first_damage.connect(on_the_first_damage)
 
 func on_more_difficulty(difficulty := 0)->void:
 
-	match difficulty:
-		6:
-			with_wolf.play()
-		11:
-			if with_wolf.playing:
-				await with_wolf.finished
-			with_gorlen.play()
-		15:
-			if with_gorlen.playing:
-				await with_gorlen.finished
-			the_final.play()
+    match difficulty:
+        6:
+            with_wolf.play()
+        11:
+            if with_wolf.playing:
+                await with_wolf.finished
+            with_gorlen.play()
+        15:
+            if with_gorlen.playing:
+                await with_gorlen.finished
+            the_final.play()
 
 
 func on_the_first_damage()->void:
-	the_first.play()
+    the_first.play()
 
 
 func _on_w_ith_gorlen_finished() -> void:
-	await get_tree().create_timer(1).timeout
-	the_final.play()
-	pass # Replace with function body.
+    await get_tree().create_timer(1).timeout
+    the_final.play()
+    pass # Replace with function body.
--- script/upgrade/sword_ability_controller.gd
+++ script/upgrade/sword_ability_controller.gd
@@ -8,70 +8,70 @@ var number=1#定义基础剑的数量
 var volume =0
 
 func _ready() -> void:
-	base_wait_time=$Timer.wait_time
-	get_node("Timer").timeout.connect(on_timer_timeoout)
-	print(GameEvent.ability_upgrade_add.connect(on_ability_upgrade_add))
-	
+    base_wait_time=$Timer.wait_time
+    get_node("Timer").timeout.connect(on_timer_timeoout)
+    print(GameEvent.ability_upgrade_add.connect(on_ability_upgrade_add))
+    
 func on_timer_timeoout():
-	var player=get_tree().get_first_node_in_group("player") as Node2D
-	var enemies= get_tree().get_nodes_in_group("enemy")
-	enemies=enemies.filter(func(enemy:Node2D):#过滤掉不在范围内的敌人
-		return enemy.global_position.distance_squared_to(player.global_position)<pow(MAX_RANGE,2)
-	
-	)
-	#get_first_node_in_group是拿到组里面的第一个节点，如果我们想要拿到player节点
-	#就要在player脚本中加入add_to_group("player")
-	enemies.sort_custom(func (a:Node2D,b:Node2D):
-		var A_distance=a.global_position.distance_squared_to(player.global_position)
-		var B_distance=b.global_position.distance_squared_to(player.global_position)
-		return A_distance< B_distance
-	
-		
-	)
-	#print("过滤+排序后敌人数量=", enemies.size())
-	var spawn_number=min(enemies.size(),number)
-	if spawn_number==0:
-		return
-	for i in range(spawn_number):
-		var sword_instance = sword_ability.instantiate() as Swordability
-		var foreground = get_tree().get_first_node_in_group("前景图层")
-		
-		# ② 等节点_ready完成再赋值，避免 nil
-		sword_instance.scale*=1.5
-		foreground.add_child(sword_instance)
-		sword_instance.hitbox_component.damage = int(Damage+randf_range(-2,4))
-		sword_instance.audio_stream_player_2d.volume_db=volume
-		if enemies.is_empty():
-			sword_instance.queue_free()
-			return
-		var enemy = enemies[i]
-		
+    var player=get_tree().get_first_node_in_group("player") as Node2D
+    var enemies= get_tree().get_nodes_in_group("enemy")
+    enemies=enemies.filter(func(enemy:Node2D):#过滤掉不在范围内的敌人
+        return enemy.global_position.distance_squared_to(player.global_position)<pow(MAX_RANGE,2)
+    
+    )
+    #get_first_node_in_group是拿到组里面的第一个节点，如果我们想要拿到player节点
+    #就要在player脚本中加入add_to_group("player")
+    enemies.sort_custom(func (a:Node2D,b:Node2D):
+        var A_distance=a.global_position.distance_squared_to(player.global_position)
+        var B_distance=b.global_position.distance_squared_to(player.global_position)
+        return A_distance< B_distance
+    
+        
+    )
+    #print("过滤+排序后敌人数量=", enemies.size())
+    var spawn_number=min(enemies.size(),number)
+    if spawn_number==0:
+        return
+    for i in range(spawn_number):
+        var sword_instance = sword_ability.instantiate() as Swordability
+        var foreground = get_tree().get_first_node_in_group("前景图层")
+        
+        # ② 等节点_ready完成再赋值，避免 nil
+        sword_instance.scale*=1.5
+        foreground.add_child(sword_instance)
+        sword_instance.hitbox_component.damage = int(Damage+randf_range(-2,4))
+        sword_instance.audio_stream_player_2d.volume_db=volume
+        if enemies.is_empty():
+            sword_instance.queue_free()
+            return
+        var enemy = enemies[i]
+        
 
-		# ③ 位置 & 朝向
-		sword_instance.global_position =enemy.global_position
-		sword_instance.global_position += Vector2.RIGHT.rotated(randf_range(0, TAU)) * 4
-		var dir_to_player = player.global_position - enemy.global_position
-		sword_instance.rotation = dir_to_player.angle()
-	
+        # ③ 位置 & 朝向
+        sword_instance.global_position =enemy.global_position
+        sword_instance.global_position += Vector2.RIGHT.rotated(randf_range(0, TAU)) * 4
+        var dir_to_player = player.global_position - enemy.global_position
+        sword_instance.rotation = dir_to_player.angle()
+    
 
 # 再拿最近的那只
-	
+    
 
-	
+    
 func on_ability_upgrade_add(upgrade:AbilityUpgrade,current_upgrade:Dictionary):
 
-	#监听所有关于剑的升级
-	if upgrade.ID=="剑的速度":
-		var persent_reduction=current_upgrade["剑的速度"]["quantity"]*.2
-		$Timer.wait_time=max(base_wait_time*(1-persent_reduction),0.2)
-		if persent_reduction==1:
-			volume-=5
-		$Timer.start()
-	if upgrade.ID=="剑的伤害":
-		Damage=(Damage+2)*1.4
-	if upgrade.ID=="剑的数量":
-		number+=1
-		volume-=2
-	pass	
-	
-	
+    #监听所有关于剑的升级
+    if upgrade.ID=="剑的速度":
+        var persent_reduction=current_upgrade["剑的速度"]["quantity"]*.2
+        $Timer.wait_time=max(base_wait_time*(1-persent_reduction),0.2)
+        if persent_reduction==1:
+            volume-=5
+        $Timer.start()
+    if upgrade.ID=="剑的伤害":
+        Damage=(Damage+2)*1.4
+    if upgrade.ID=="剑的数量":
+        number+=1
+        volume-=2
+    pass	
+    
+    
