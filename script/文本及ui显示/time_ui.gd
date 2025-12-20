68390a9d62d0cc273ec2d19d80deba871917991f
monolith-Epee <3572871617@qq.com>
2025-12-20 14:49:00 +0800

    .
    .

--- script/文本及ui显示/time_ui.gd
+++ script/文本及ui显示/time_ui.gd
@@ -5,10 +5,10 @@ extends CanvasLayer
 @onready var label: Label = %Label
 
 func _process(_delta: float) -> void:
-	var time_elapse=game_time_manager.get_time_elaspsed()
-	label.text=format_second(time_elapse)#传入剩余时间
+    var time_elapse=game_time_manager.get_time_elaspsed()
+    label.text=format_second(time_elapse)#传入剩余时间
 func format_second(seconds:float):
-	var minute=int(floor(seconds/60))
-	var remaining_second=seconds-(minute*60)
-	return str(minute)+":"+str("%02d"%int(floor(remaining_second)))
-	
+    var minute=int(floor(seconds/60))
+    var remaining_second=seconds-(minute*60)
+    return str(minute)+":"+str("%02d"%int(floor(remaining_second)))
+    
